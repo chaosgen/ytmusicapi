chaosgen/ytmusicapi

commit 1b062fc3b4bb77f917abc875ac9bbcc2e4a2a06f

Fix 'secondSubtitle' KeyError for some playlists (#399)
* Fix 'secondSubtitle' KeyError for some playlists

* Feedback.

ytmusicapi/mixins/playlists.py
@@ -132,14 +132,16 @@ class PlaylistsMixin:
             if run_count == 5:
                 playlist['year'] = nav(header, SUBTITLE3)
 
-        second_subtitle_runs = header['secondSubtitle']['runs']
-
-        has_views = (len(second_subtitle_runs) > 3) * 2
-        playlist['views'] = None if not has_views else to_int(second_subtitle_runs[0]['text'])
-        has_duration = (len(second_subtitle_runs) > 1) * 2
-        playlist['duration'] = None if not has_duration else second_subtitle_runs[has_views + has_duration]['text']
-        song_count = second_subtitle_runs[has_views + 0]['text'].split(" ")
-        song_count = to_int(song_count[0]) if len(song_count) > 1 else 0
+        playlist['views'] = None
+        playlist['duration'] = None
+        if 'runs' in header['secondSubtitle']:
+            second_subtitle_runs = header['secondSubtitle']['runs']
+            has_views = (len(second_subtitle_runs) > 3) * 2
+            playlist['views'] = None if not has_views else to_int(second_subtitle_runs[0]['text'])
+            has_duration = (len(second_subtitle_runs) > 1) * 2
+            playlist['duration'] = None if not has_duration else second_subtitle_runs[has_views + has_duration]['text']
+        
+        song_count = len(results['contents'])
         playlist['trackCount'] = song_count
 
         request_func = lambda additionalParams: self._send_request(endpoint, body, additionalParams)

commit 783a0ee74f6f754351d5658c10f1efeb1ec3d4d5

fix empty liked songs crash caused by #399

ytmusicapi/mixins/playlists.py
@@ -140,8 +140,11 @@ class PlaylistsMixin:
             playlist['views'] = None if not has_views else to_int(second_subtitle_runs[0]['text'])
             has_duration = (len(second_subtitle_runs) > 1) * 2
             playlist['duration'] = None if not has_duration else second_subtitle_runs[has_views + has_duration]['text']
-        
-        song_count = len(results['contents'])
+            song_count = second_subtitle_runs[has_views + 0]['text'].split(" ")
+            song_count = to_int(song_count[0]) if len(song_count) > 1 else 0
+        else:
+            song_count = len(results['contents'])
+
         playlist['trackCount'] = song_count
 
         request_func = lambda additionalParams: self._send_request(endpoint, body, additionalParams)

tests/test.py
@@ -388,12 +388,15 @@ class TestYTMusic(unittest.TestCase):
     # PLAYLISTS
     ###############
 
-    def test_get_foreign_playlist(self):
+    def test_get_playlist_foreign(self):
         self.assertRaises(Exception, self.yt.get_playlist, "PLABC")
         playlist = self.yt.get_playlist(sample_playlist, limit=300, suggestions_limit=7)
         self.assertGreater(len(playlist['duration']), 5)
         self.assertGreater(len(playlist["tracks"]), 200)
         self.assertNotIn("suggestions", playlist)
+
+        self.yt.get_playlist("RDATgXd-")
+        self.assertGreaterEqual(len(playlist["tracks"]), 100)
 
         playlist = self.yt_oauth.get_playlist("PLj4BSJLnVpNyIjbCWXWNAmybc97FXLlTk",
                                               limit=None,
@@ -401,7 +404,7 @@ class TestYTMusic(unittest.TestCase):
         self.assertGreater(len(playlist["tracks"]), 200)
         self.assertEqual(len(playlist["related"]), 0)
 
-    def test_get_owned_playlist(self):
+    def test_get_playlist_owned(self):
         playlist = self.yt_brand.get_playlist(config["playlists"]["own"],
                                               related=True,
                                               suggestions_limit=21)